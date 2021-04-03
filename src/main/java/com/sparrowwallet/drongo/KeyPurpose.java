2fc1b0fc91bc775340f9b0ec9b298ac9266ec7e0
hashat <hashat@sent.at>
2021-04-03 18:52:02 +0200

    First working version - minus Wallet (due to tons of debug)

--- src/main/java/com/sparrowwallet/drongo/KeyPurpose.java
+++ src/main/java/com/sparrowwallet/drongo/KeyPurpose.java
@@ -2,26 +2,23 @@ package com.sparrowwallet.drongo;
 
 import com.sparrowwallet.drongo.crypto.ChildNumber;
 
-public enum KeyPurpose {
-    RECEIVE(new ChildNumber(20)), CHANGE(new ChildNumber(21));
+public class KeyPurpose {
 
     private final ChildNumber pathIndex;
 
-    KeyPurpose(ChildNumber pathIndex) {
-        this.pathIndex = pathIndex;
+    public KeyPurpose(int index) {
+        this.pathIndex = new ChildNumber(index);
     }
 
     public ChildNumber getPathIndex() {
         return pathIndex;
     }
 
-    public static KeyPurpose fromChildNumber(ChildNumber childNumber) {
-        for(KeyPurpose keyPurpose : values()) {
-            if(keyPurpose.getPathIndex().equals(childNumber)) {
-                return keyPurpose;
-            }
-        }
-
-        return null;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        KeyPurpose that = (KeyPurpose) o;
+        return this.pathIndex.i() == that.pathIndex.i();
     }
 }
